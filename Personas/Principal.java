1acdf563c66e727041154ee8bef8342b3a284241
david <dafuentesc@unal.edu.co>
2024-08-19 14:03:02 -0500

    subiendo el taller 4

---
+++ Personas/Principal.java
@@ -0,0 +1,10 @@
+package taller_4.Personas;
+
+public class Principal {
+    
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal; 
+        miVentanaPrincipal= new VentanaPrincipal(); 
+        miVentanaPrincipal.setVisible(true); 
+    }
+}
